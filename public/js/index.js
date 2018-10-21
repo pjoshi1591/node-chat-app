9bc2afb29ff712673bbf4a27dcad6fe8ff62fd0f
tejprakash <tejprakash1590@gmail.com>
2018-10-20 20:08:28 -0500

    Add join page that submits to chat.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,21 @@
 var socket = io();
+
+function scrollToBottom () {
+  //Selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+
+  //Height
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight+scrollTop+newMessageHeight+lastMessageHeight>=scrollHeight){
+    messages.scrollTop(scrollHeight);
+  }
+}
 socket.on('connect', function()  {
   console.log('Connected to server');
 });
@@ -16,7 +33,8 @@ socket.on('newMessage', function (message) {
     createdAt:formattedTime
   });
 
-  jQuery('#message').append(html);
+  jQuery('#messages').append(html);
+  scrollToBottom();
 
 });
 
@@ -28,7 +46,8 @@ socket.on('newLocationMessage', function (message) {
     url:message.url,
     createdAt:formattedTime
   });
-  jQuery('#message').append(html);
+  jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (e) {
